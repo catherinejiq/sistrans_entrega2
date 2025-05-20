5223e8f22a708a631359cc80626110cbcc9a14e2
Paola Jimenez <p.jimenezj@uniandes.edu.co>
2025-05-20 18:42:24 -0500

    RF4 y RF5

---
+++ epsandes_entrega4/src/main/java/uniandes/edu/co/demo/modelo/Medico.java
@@ -0,0 +1,69 @@
+package uniandes.edu.co.demo.modelo;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.ToString;
+
+@Document(collection = "medicos_collection")
+@ToString
+public class Medico {
+
+    @Id
+    private String id; 
+    private String identificacion;
+    private String nombre;
+    private String numRegistro;
+    private String especialidad;
+
+    public Medico() {}
+
+    public Medico(String identificacion, String nombre, String numRegistro, String especialidad) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.numRegistro = numRegistro;
+        this.especialidad = especialidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumRegistro() {
+        return numRegistro;
+    }
+
+    public void setNumRegistro(String numRegistro) {
+        this.numRegistro = numRegistro;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+}
