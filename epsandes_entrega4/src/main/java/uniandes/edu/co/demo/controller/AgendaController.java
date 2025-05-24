433474ea457ed7fd1362124837f7528186f95f1b
jeronimov10 <j.vasquezp2@uniandes.edu.co>
2025-05-24 16:34:18 -0500

    RF7 y RFC1

--- epsandes_entrega4/src/main/java/uniandes/edu/co/demo/controller/AgendaController.java
+++ epsandes_entrega4/src/main/java/uniandes/edu/co/demo/controller/AgendaController.java
@@ -1,12 +1,14 @@
 package uniandes.edu.co.demo.controller;
+
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.web.server.ResponseStatusException;
 
 import uniandes.edu.co.demo.modelo.Cita;
@@ -28,22 +30,47 @@ public class AgendaController {
     @Autowired
     private OrdenServicioRepository ordenRepo;
 
-    /**
-     * RF7: Agendar un servicio de salud (requiere orden salvo "Consulta general"/"URGENCIA").
-     */
+    
+    // RFC1: Consultar disponibilidad de un servicio
+   
+    @GetMapping("/agenda/{idServicio}")
+    public ResponseEntity<List<Disponibilidad>> consultarAgenda(
+            @PathVariable int idServicio) {
+
+        
+        Date ahora = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(ahora);
+        cal.add(Calendar.WEEK_OF_YEAR, 4);
+        Date dentroDe4Semanas = cal.getTime();
+
+        
+        List<Disponibilidad> franjas = dispRepo
+            .findByIdServicioAndEstadoAndFechaHoraInicioBetween(
+                idServicio,
+                "LIBRE",
+                ahora,
+                dentroDe4Semanas
+            );
+
+        return ResponseEntity.ok(franjas);
+    }
+
+    
+    // RF7: Agendar un servicio de salud
+    
     @PostMapping("/agendar")
     public ResponseEntity<?> agendarCita(@RequestBody Map<String,String> payload) {
-        String dispId  = payload.get("disponibilidadId");
-        String afiId   = payload.get("afiliadoId");
-        String ordId   = payload.get("ordenId"); 
+        String dispId = payload.get("disponibilidadId");
+        String afiId  = payload.get("afiliadoId");
+        String ordId  = payload.get("ordenId");
 
-       
+        
         Disponibilidad d = dispRepo.findByIdAndEstado(dispId, "LIBRE")
             .orElseThrow(() -> new ResponseStatusException(
                 HttpStatus.CONFLICT, "Franja no disponible o ya ocupada"
             ));
 
-        
         
         if (d.getIdServicio() != 1 && d.getIdServicio() != 2) {
             if (ordId == null || !ordenRepo.existsById(ordId)) {
@@ -67,6 +94,7 @@ public class AgendaController {
         }
         citaRepo.save(c);
 
+        
         return ResponseEntity.ok(Map.of("message","Cita agendada"));
     }
 }
